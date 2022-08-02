9394a38cddf763a26afa9bfcef0447eb29cfda92
Nguyen The Manh <themanh.nguyen@petronas.com>
2022-08-02 21:53:43 +0700

    change language by onclick

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const { t } = useTranslation()
+  const [lang, setLang] = useState('en')
+  const { t, i18n } = useTranslation()
+  const changeLanguage = () => {
+    i18n.changeLanguage('vi')
+  }
+  useEffect(() => {
+    i18n.changeLanguage(lang)
+  }, [lang])
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +29,21 @@ function App() {
           Learn React
         </a>
       </header>
+      <button
+        onClick={changeLanguage}
+        type="button"
+        className="text-white absolute top-0"
+      >
+        Change Vietnamese language
+      </button>
+      <select
+        value={lang}
+        onChange={e => setLang(e.target.value)}
+        className="text-blue absolute top-0 left-0"
+      >
+        <option value="en">Eng-US</option>
+        <option value="vi">Tiếng việt</option>
+      </select>
     </div>
   )
 }
