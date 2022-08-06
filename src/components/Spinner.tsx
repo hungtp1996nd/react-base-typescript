78c11f1839ead568cd6f6ba78648d352ba495c17
Truong Phuc Hung <hungtp1996nd@gmail.com>
2022-08-06 23:57:48 +0700

    customize spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { ClimbingBoxLoader } from 'react-spinners'
 
 const Spinner = () => {
-  return <div>Loading...</div>
+  return (
+    <div className="flex items-center justify-center">
+      <ClimbingBoxLoader color="#5EF2D5" size={20} />
+    </div>
+  )
 }
 
 export default Spinner
